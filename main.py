c599f40848126ecc04008f5258a51baebf418478
Elijah Tarr <elijahotarr@gmail.com>
2023-06-22 21:45:25 -0700

    add simple memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from langchain.chains import LLMChain, SimpleSequentialChain, SequentialChain
 from langchain import PromptTemplate, OpenAI, LLMChain
+from langchain.memory import SimpleMemory
 from langchain.llms import type_to_cls_dict
 import json
 
@@ -25,12 +26,14 @@ def get_llm_chain(cfg):
     llm = get_llm(cfg["llm"])
     prompt = get_prompt(cfg["prompt"])
     output_key = cfg.get("output_key")
-    llm_chain = LLMChain(llm=llm, prompt=prompt, output_key=output_key)
+    memory = try_memory(cfg)
+    llm_chain = LLMChain(llm=llm, prompt=prompt, output_key=output_key, memory=memory)
     return llm_chain
 
 def get_simple_sequential_chain(cfg):
     chains = [get_chain(chain) for chain in cfg["chains"]]
-    ss_chain = SimpleSequentialChain(chains=chains, verbose=True)
+    memory = try_memory(cfg)
+    ss_chain = SimpleSequentialChain(chains=chains, verbose=True, memory=memory)
     return ss_chain
 
 def get_prompt(cfg):
@@ -41,5 +44,20 @@ def get_sequential_chain(cfg):
     chains = [get_chain(chain) for chain in cfg["chains"]]
     input_variables = cfg["input_variables"]
     output_variables = cfg["output_variables"]
-    seq_chain = SequentialChain(chains=chains, input_variables=input_variables, output_variables=output_variables, verbose=True)
+    memory = try_memory(cfg)
+    seq_chain = SequentialChain(chains=chains, input_variables=input_variables, output_variables=output_variables, verbose=True, memory=memory)
     return seq_chain
+
+def try_memory(cfg):
+    if "memory" in cfg:
+        return get_memory(cfg["memory"])
+    return None
+
+def get_memory(cfg):
+    if cfg["type"] == "SimpleMemory":
+        return get_simple_memory(cfg)
+
+def get_simple_memory(cfg):
+    memories = cfg["memories"]
+    simple_memory = SimpleMemory(memories=memories)
+    return simple_memory
