863f4344d6b8412b780fa9df34ca0fe524797f0e
Elijah Tarr <elijahotarr@gmail.com>
2023-07-06 14:08:26 -0700

    Align validation with current progress

--- src/langchain_interpreter/__init__.py
+++ src/langchain_interpreter/__init__.py
@@ -1,4 +1,4 @@
 """Main entrypoint into package."""
 
-from langchain_interpreter.validation import validate_chain
+from langchain_interpreter.validation import validate_chain, validate_file
 from langchain_interpreter.main import chain_from_file, chain_from_str
--- src/langchain_interpreter/validation.py
+++ src/langchain_interpreter/validation.py
@@ -1,12 +1,17 @@
 from referencing import Registry, Resource
 from jsonschema import Draft202012Validator
+import json
 
 
 def get_validator():
-    chain_schema_resource = Resource.from_contents(chain_schema)
-    llm_schema_resource = Resource.from_contents(llm_schema)
+    chain_resource = Resource.from_contents(chain_schema)
+    llm_resource = Resource.from_contents(llm_schema)
+    prompt_resource = Resource.from_contents(prompt_schema)
+    memory_resource = Resource.from_contents(memory_schema)
 
-    registry = chain_schema_resource @ (llm_schema_resource @ Registry())
+    registry = chain_resource @ (
+        llm_resource @ (prompt_resource @ (memory_resource @ Registry()))
+    )
 
     validator = Draft202012Validator(chain_schema, registry=registry)
 
@@ -16,6 +21,12 @@ def get_validator():
 def validate_chain(json):
     validator = get_validator()
     validator.validate(json)
+
+
+def validate_file(path):
+    with open(path) as f:
+        cfg = json.load(f)
+    validate_chain(cfg)
 
 
 chain_schema = {
@@ -35,7 +46,24 @@ chain_schema = {
             "description": "A list of chains to be executed in the sequential chain.",
             "items": {"$ref": "https://example.com/template.schema.json"},
         },
+        "prompt": {"$ref": "https://example.com/prompt.schema.json"},
+        "memory": {"$ref": "https://example.com/memory.schema.json"},
+        "input_variables": {
+            "type": "array",
+            "description": "List of input variables to the chain",
+            "items": {"type": "string"},
+        },
+        "output_variables": {
+            "type": "array",
+            "description": "List of output variables for the chain",
+            "items": {"type": "string"},
+        },
+        "output_key": {
+            "type": "string",
+            "description": "The key for the output of this specific chain.",
+        },
     },
+    "additionalProperties": False,
     "required": ["type"],
 }
 
@@ -55,5 +83,57 @@ llm_schema = {
             "description": "The arguments of the llm to be passed in",
         },
     },
+    "additionalProperties": False,
+    "required": ["type"],
+}
+
+prompt_schema = {
+    "$id": "https://example.com/prompt.schema.json",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "LangChain Prompt Template",
+    "description": "A langchain prompt template",
+    "type": "object",
+    "properties": {
+        "template": {
+            "type": "string",
+            "description": "The template string for the prompt template.",
+        },
+        "input_variables": {
+            "type": "array",
+            "description": "A list of the input variables for the template",
+            "items": {"type": "string"},
+        },
+    },
+    "additionalProperties": False,
+    "required": ["template"],
+}
+
+memory_schema = {
+    "$id": "https://example.com/memory.schema.json",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "LangChain Memory",
+    "description": "A langchain memory object",
+    "type": "object",
+    "properties": {
+        "type": {
+            "type": "string",
+            "description": "The type of memory to use.",
+        },
+        "memories": {
+            "type": "object",
+            "description": "A kv store of the memories for simple memory.",
+            "additionalProperties": {"type": "string"},
+        },
+        "context": {
+            "type": "array",
+            "description": "List of conversation inputs and outputs.",
+            "items": {"type": "object"},
+        },
+        "history": {
+            "type": "string",
+            "description": "String of previous conversation.",
+        },
+    },
+    "additionalProperties": False,
     "required": ["type"],
 }
